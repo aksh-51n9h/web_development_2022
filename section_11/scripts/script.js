ca388c1bc768ce8011796047ffb6a7e7253522f8
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-17 22:59:11 +0530

    complete lecture 145 challenges

--- section_11/scripts/script.js
+++ section_11/scripts/script.js
@@ -3,3 +3,6 @@ document.querySelector("li a").style.color = "green";
 
 // #144 Challenge 2
 document.querySelector("button").style.color = "yellow";
+
+// #145 Challenge 1
+document.querySelector("h1").classList.add("huge");
