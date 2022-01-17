bea4b25d6ba6a210b849abe976befc0d37b21ac6
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-17 23:04:33 +0530

    complete lecture 146

--- section_11/scripts/script.js
+++ section_11/scripts/script.js
@@ -6,3 +6,10 @@ document.querySelector("button").style.color = "yellow";
 
 // #145 Challenge 1
 document.querySelector("h1").classList.add("huge");
+
+// #146 textContent = only gives text inside a tag
+var textContentResult = document.querySelector("h1").textContent;
+var innerHTMLResult = document.querySelector("h1").innerHTML;
+
+console.log("using textContent: " + textContentResult);
+console.log("using innerHTML: " + innerHTMLResult);
