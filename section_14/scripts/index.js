d494712b97bc39794696bd56e2b9ddd02a0e0829
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-18 23:33:13 +0530

    finish section 14

--- section_14/scripts/index.js
+++ section_14/scripts/index.js
@@ -4,3 +4,20 @@ $("h1").addClass("big-title");
 // #179 how to check class using jQuery
 console.log($("h1").hasClass("big-title"));
 
+// #180 change text
+$("h1").text("Text Changed");
+
+// #181 manipulating attribute
+console.log($("button").attr("type"));
+
+$("button").attr("type", "submit");
+
+// #182 add event listeners
+// $("h1").click(function () { 
+//     alert("title cliked");
+// });
+
+// #184 animate using jQuery
+$("button").on("click", function () { 
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
