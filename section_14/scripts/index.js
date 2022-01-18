3ca2b8ce8455b72b8edde96a601f7c926a3ac52a
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-18 23:15:39 +0530

    complete lecture 179

--- section_14/scripts/index.js
+++ section_14/scripts/index.js
@@ -1,1 +1,6 @@
-$("h1").css("color", "red");
+// #179 how to add class using jQuery
+$("h1").addClass("big-title");
+
+// #179 how to check class using jQuery
+console.log($("h1").hasClass("big-title"));
+
