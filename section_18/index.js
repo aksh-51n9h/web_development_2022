ce48ceadd1de62c506ef2d2264f8102d09f085d5
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-19 17:31:29 +0530

    complete lecture 224 challenge

---
+++ section_18/index.js
@@ -0,0 +1,13 @@
+var superheroes = require("superheroes");
+
+var randomName = superheroes.random();
+
+console.log(randomName);
+
+// #224 challenge 1
+
+var supervillains = require("supervillains");
+
+var randomSupervillainsName = supervillains.random();
+
+console.log(randomSupervillainsName);
