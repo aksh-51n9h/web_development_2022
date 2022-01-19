50564a30ff641448622ca40ed4cd9b7ed1d2df9a
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-19 18:31:34 +0530

    complete lecture 230 challenge

--- section_19/my-express-server/server.js
+++ section_19/my-express-server/server.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("<h1>My first express server</h1>");
+});
+
+app.get("/contact", function (req, res) {
+    res.send("Contact: singh2104.akshay@gmail.com");
+});
+
+// #230 challenge 1
+app.get("/about", function (req, res) {
+    res.send("Author: Akshay");
+});
+
+app.listen(3000, function () { 
+    console.log("server started at port 3000");
+});
