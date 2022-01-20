cbb943682d3816604811fbe24e18c95d66538d00
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-21 00:04:38 +0530

    install body-parser

--- section_19/calculator/calculator.js
+++ section_19/calculator/calculator.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function (req, res) {
+    // console.log(__dirname+"/index.html");
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function (req, res) {
+    res.send("Everything is working fine.");
+});
+
 app.listen(3000, function () { 
     console.log("calculator server started at port 3000");
 });
