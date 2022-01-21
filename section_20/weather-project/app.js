53882fe689590feb1d39cc866657870866617eb2
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-22 01:10:20 +0530

    complete lecture 245 challenge 1

--- section_20/weather-project/app.js
+++ section_20/weather-project/app.js
@@ -4,7 +4,7 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) { 
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=2b471d8d705491f5b160f0d5186293be&q=India&units=metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=2b471d8d705491f5b160f0d5186293be&units=metric&id=1267995";
 
     https.get(url, function (response) { 
         // console.log(response.statusCode);
@@ -12,11 +12,15 @@ app.get("/", function (req, res) {
         response.on("data", function (data) { 
             // console.log(data);
             var weatherData = JSON.parse(data);
-            console.log(weatherData.weather[0].description);
+            var temp = weatherData.main.temp;
+            var weatherDescription = weatherData.weather[0].description;
+
+            console.log(weatherDescription);
+            res.send("<h1>The temperature in India is "+ temp + " degree Celcius.</h1>");
         });
     });
 
-    res.send("working");
+    // res.send("working");
 });
 
 app.listen(3000, function () { 
