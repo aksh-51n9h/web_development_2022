cfc8b95d7e0f7ba3cacaf2abf0741d97baa8dafa
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-22 01:02:02 +0530

    complete lecture 244 challenge 1

--- section_20/weather-project/app.js
+++ section_20/weather-project/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const https = require("https");
+
 const app = express();
 
 app.get("/", function (req, res) { 
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=2b471d8d705491f5b160f0d5186293be&q=India&units=metric";
+
+    https.get(url, function (response) { 
+        // console.log(response.statusCode);
+
+        response.on("data", function (data) { 
+            // console.log(data);
+            var weatherData = JSON.parse(data);
+            console.log(weatherData.weather[0].description);
+        });
+    });
+
     res.send("working");
 });
 
