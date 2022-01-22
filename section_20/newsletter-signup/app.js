a29898f17716176edcf497356f423f2e8b73d3a6
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-22 15:01:28 +0530

    start todo-list project

--- section_20/newsletter-signup/app.js
+++ section_20/newsletter-signup/app.js
@@ -5,11 +5,19 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodeParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname+"/signup.html");
+    res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3000, function () { 
+app.post("/", function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+
+    console.log(name + " " + email);
+});
+
+app.listen(3000, function () {
     console.log("newsletter-signup server up and ready to serve.");
 });
---
+++ section_20/todo-list-project/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) { 
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(3000, function () { 
+    console.log("todo-list server started at port 3000.");
+});
