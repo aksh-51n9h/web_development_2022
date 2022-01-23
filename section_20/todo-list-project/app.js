3855c645f8f5a6fdd2417d418841efaffb394034
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-23 15:08:57 +0530

    complete lecture 268 challenge

--- section_20/todo-list-project/app.js
+++ section_20/todo-list-project/app.js
@@ -4,11 +4,33 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) { 
-    res.sendFile(__dirname+"/index.html");
+var items = [];
+
+app.get("/", function (req, res) {
+    // res.sendFile(__dirname+"/index.html");
+
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { dayName: day , items: items});
 });
 
-app.listen(3000, function () { 
+app.post("/", function (req, res) { 
+    var item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
     console.log("todo-list server started at port 3000.");
 });
