1821e9a1daba884ea4acf8a985b706d2fac21d87
aksh-51n9h <singh2104.akshay@gmail.com>
2022-01-29 12:31:06 +0530

    implement level 1 authentication

--- section_32/secrets_project/app.js
+++ section_32/secrets_project/app.js
@@ -29,17 +29,51 @@ app.route("/login")
     })
 
     .post(function (req, res) {
+        const email = req.body.username;
+        const password = req.body.password;
 
+        User.findOne({ email: email }, function (err, foundUser) {
+            if (err) {
+                let message = "Error occured! No user found.";
+                res.send(message);
+            } else {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                } else {
+                    let message = "Error occured! Wrong password";
+                    res.send(message);
+                }
+            }
+        });
     });
 
 app.route("/register")
     .get(function (req, res) {
         res.render("register");
     })
-    
-    .post(function (req, res) { 
-        
+
+    .post(function (req, res) {
+        const email = req.body.username;
+        const password = req.body.password;
+
+        const newUser = new User({
+            email: email,
+            password: password
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                let message = "Error occured! Unable to register.";
+                res.send(message);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
+
+app.get("/logout", function (req, res) {
+    res.render("home");
+});
 
 
 app.listen(3000, function () {
