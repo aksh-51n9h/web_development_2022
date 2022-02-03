59d3c00a0ec698627e85a40fe96921eb0a02d710
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-03 18:11:14 +0530

    complete javascript expression in JSX

--- section_33/my-first-react-app/src/03_javascript_expression_in_JSX.js
+++ section_33/my-first-react-app/src/03_javascript_expression_in_JSX.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const firstName = "Akshay";
+const lastName = "Singh";
 const myLuckyNumber = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello Akshay</h1>
+    <h1>
+      Hello {firstName} {lastName}
+    </h1>
     <p>My lucky number is {myLuckyNumber}</p>
   </div>,
   document.getElementById("root")
