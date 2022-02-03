487ecf9d613fe0d2045cf556683156264cc956cc
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-03 11:23:54 +0530

    complete introduction to  jsx

--- section_33/my-first-react-app/src/index.js
+++ section_33/my-first-react-app/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+// ReactDOM.render(WHAT TO SHOW, WHERE TO SHOW, CALLBACK)
+
+ReactDOM.render(<h1>Hello to React JS</h1>, document.getElementById("root"));
+
+// render method accpets only single element
+
+//inavild render method
+
+// ReactDOM.render(<h1>Hello to React JS</h1><p>React is awesome</p>, document.getElementById("root"));
+
+// in the above example we are trying to give two elements which is not allowed so that's why it is invalid.
+
+// we can enclose them in one div
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  <div>
+    <h1>Hello to React JS</h1>
+    <p>React is awesome</p>
+  </div>, document.getElementById("root"));
