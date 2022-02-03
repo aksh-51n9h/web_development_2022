8c6dbfce9c0de0011e4fd8937fa34fd01f8b6c98
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-03 17:57:34 +0530

    refactor filename and start javascript expression in JSX

---
+++ section_33/my-first-react-app/src/03_javascript_expression_in_JSX.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const myLuckyNumber = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello Akshay</h1>
+    <p>My lucky number is {myLuckyNumber}</p>
+  </div>,
+  document.getElementById("root")
+);
