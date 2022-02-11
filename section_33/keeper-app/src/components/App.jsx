0618f36eb4108618cf70bdf28225dbfabe74f0a5
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-11 11:11:13 +0530

    complete keeper app challenge 2

--- section_33/keeper-app/src/components/App.jsx
+++ section_33/keeper-app/src/components/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes.js";
 
 export default App;
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map((note)=>(<Note key={note.key} title={note.title} content={note.content} />))}
         <Footer />
     </div>;
 }
